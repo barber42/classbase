f7802618e156acd83efed2c38477d05899f0ce38
barber42 <bkbarber42@gmail.com>
2020-03-23 16:00:34 -0600

    Front end will display book table data, need to be able to get Author to come up.

--- dbW2020/bookapp/views.py
+++ dbW2020/bookapp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-
+from .models import Book, Author, Genre, Transaction
 # Create your views here.
 
 def book_list(request):
-	return render(request, 'bookapp/book_list.html', {})
+	books = Book.objects.all()
+	return render(request, 'bookapp/book_list.html', {'books' : books})
