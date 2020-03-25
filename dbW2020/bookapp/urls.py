74a491a9b1b47718381e54b3332afa86fe781996
barber42 <bkbarber42@gmail.com>
2020-03-25 12:16:47 -0600

    All basic functions operational

--- dbW2020/bookapp/urls.py
+++ dbW2020/bookapp/urls.py
@@ -1,9 +1,14 @@
 from django.urls import path
 from . import views
+from django.contrib import admin
+from django.contrib.auth import views as authviews
 
 urlpatterns = [
 	path('', views.book_list, name='book_list'),
 	path('book/new/', views.book_new, name='book_new'),
 	path('author/new', views.author_new, name='author_new'),
 	path('transaction/new', views.transaction_new, name='transaction_new'),
+	path('transaction/list/', views.transaction_list, name='transaction_list'),
+	path('accounts/login/', authviews.LoginView.as_view(), name='login'),
+	path('accounts/logout/', authviews.LogoutView.as_view(next_page='/'), name='logout'),
 ]
--- dbW2020/bookapp/views.py
+++ dbW2020/bookapp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from .models import Book, Author, Genre, Transaction
 from .forms import BookForm, AuthorForm, TransactionForm
 from django.shortcuts import redirect
+from decimal import Decimal
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,6 +11,12 @@ def book_list(request):
 	books = Book.objects.all()
 	return render(request, 'bookapp/book_list.html', {'books' : books})
 
+@login_required
+def transaction_list(request):
+	transactions = Transaction.objects.all()
+	return render(request, 'bookapp/transaction_list.html', {'transactions' : transactions})
+
+@login_required
 def book_new(request):
 	if request.method == "POST":
 		form = BookForm(request.POST)
@@ -32,13 +40,29 @@ def author_new(request):
 		form = AuthorForm()			
 	return render(request, 'bookapp/author_edit.html', {'form': form})
 
+@login_required
 def transaction_new(request):
 	if request.method == "POST":
 		form =TransactionForm(request.POST)
 		if form.is_valid():
 			transaction = form.save(commit=False)
+			
+			total = Decimal.from_float(0.00)
+			uname = request.user
+			transaction.total = total
+			transaction.userid = uname 	
+			
 			transaction.save()
 			form.save_m2m()
+				
+			for book in transaction.books.all():
+				total = total + book.price
+				book.copies -= 1
+				book.save()				
+			
+			transaction.total = total
+			transaction.save()
+			
 			return redirect('/')	
 	else:
 		form = TransactionForm()			
